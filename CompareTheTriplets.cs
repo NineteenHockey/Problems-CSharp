f706fce77af70049e4134381154423177d806e4e
NineteenHockey <nineteenhockey@hot.ee>
2021-08-07 23:17:49 +0300

    Create CompareTheTriplets.cs

---
+++ CompareTheTriplets.cs
@@ -0,0 +1,48 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b) {
+        int sumA,sumB;
+        sumA=sumB=0;
+        for (int i=0; i<3;i++)
+            {
+                if (a[i]>b[i])
+                    sumA++;
+                else if (b[i]>a[i])
+                    sumB++;
+
+            }
+            return new List<int>{sumA,sumB};
+
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
